49541c3540fe408918f7cc63679a0d3bf9827e2f
dalalalis <dasabah@gmail.com>
2022-09-27 17:47:49 +0300

    corrected mistakes

--- functions.py
+++ functions.py
@@ -7,6 +7,8 @@ def number_of_authors(book):
         numberofauthors = len(book["authors"])
         return numberofauthors
 
+    #return return len(book["author"])
+
 
 
 print(number_of_authors(books[0]))
@@ -30,9 +32,9 @@ print(get_book_by_id(38, books))
 # adds the summary to the book dictionary
 # return the book dictionary
 def add_summary_to_book(summary, book):
-    for book in books:
-        book ["summary"]=summary
-        return book
+    #not loop because one book only
+       return book ["summary"]=summary
+
 
 
 print(add_summary_to_book("this is a good book about", books[0]))
@@ -46,10 +48,15 @@ print(add_summary_to_book("this is a good book about", books[0]))
 
 
 def get_book_property(property, book):
-    for book in books:
+    '''for book in books:
         if property in book: 
-            bookvalue=books.get["property"]
-    return bookvalue
+        bookvalue=books.get["property"]
+    return bookvalue 
+    this is wrong because we dont need a loop its one book '''
+    return book[property]
+    #return book.get(property) better option it has added value of validation 
+    #so if the property doesnt exist it will return none instead of giving an erroe
+
 
 print(get_book_property("color", books[0]))
 print(get_book_property("title", books[0]))
@@ -63,11 +70,13 @@ print(get_book_property("title", books[0]))
 
 def calculate_not_available_books(books):
     unavailable_books=[]
-        if books in books:
-            return none
-        else:
-            return unavailable_books.append[books]
+    for book in books:
+        if books["available"] != True:
+            unavailable_books.append[book]
+    return unavailable_books
             
+#instead of != we could write 
+# if not books["available"]: or if books ["available"] is True:
 
 
 print(calculate_not_available_books(books))
@@ -78,7 +87,9 @@ print(calculate_not_available_books(books))
 # recieves a list of book dictionaries
 # returns the book dictionary that contains an author with the author name provided
 def get_book_by_author_name(author_name, books):
-    ...
-
+    for book in books:
+        for author in book["author"]:
+         if author_name == author["name"]:
+            return book 
 
 print(get_book_by_author_name("Neil Gaiman", books))
